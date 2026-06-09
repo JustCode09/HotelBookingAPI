aa2c401bf554ea822029515fda88e5718d9deab6
JustCode09 <Professionalacc011@gmail.com>
2026-06-09 11:36:03 +0545

    Added Room and Booking models

---
+++ Models/Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBookingAPI.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string? Status { get; set; }
+    }
+}

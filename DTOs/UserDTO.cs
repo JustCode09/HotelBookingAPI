5c5d434e51913231914122113c0dba85d7370712
JustCode09 <Professionalacc011@gmail.com>
2026-06-10 12:26:53 +0545

    Added Auth DTOs

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Role { get; set; }
+    }
+}

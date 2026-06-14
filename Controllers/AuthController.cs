e521bf403a6eb8c92c45a22cd70e0b613446a0a3
JustCode09 <Professionalacc011@gmail.com>
2026-06-14 11:44:47 +0545

    Added AuthController with Register and Login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,42 @@
-﻿namespace HotelBookingAPI.Controllers
+﻿using HotelBookingAPI.DTOs;
+using HotelBookingAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingAPI.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        // POST api/auth/register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            var result = await _authService.Register(registerDTO);
+
+            if (result == "Email already exists")
+                return BadRequest(result);
+
+            return Ok(new { token = result });
+        }
+
+        // POST api/auth/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            var result = await _authService.Login(loginDTO);
+
+            if (result == "Invalid email or password")
+                return BadRequest(result);
+
+            return Ok(new { token = result });
+        }
     }
 }

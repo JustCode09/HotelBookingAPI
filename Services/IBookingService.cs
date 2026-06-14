88839a65ea965d8eda6ca471e4bce038040cc188
JustCode09 <Professionalacc011@gmail.com>
2026-06-14 12:13:30 +0545

    Added IBookingService interface

---
+++ Services/IBookingService.cs
@@ -0,0 +1,12 @@
+﻿using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Services
+{
+    public interface IBookingService
+    {
+        Task<List<Booking>> GetAllBookings();
+        Task<Booking?> GetBookingById(int id);
+        Task<Booking> CreateBooking(Booking booking);
+        Task<bool> CancelBooking(int id);
+    }
+}

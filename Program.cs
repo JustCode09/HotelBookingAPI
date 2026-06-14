5802259a7632d78fc5eef8ad2f481dc8f048a7e5
JustCode09 <Professionalacc011@gmail.com>
2026-06-14 11:52:53 +0545

    JWT Authentication working - Register and Login

--- Program.cs
+++ Program.cs
@@ -83,7 +83,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hotel Booking API v1");
-        c.RoutePrefix = string.Empty;
+        c.RoutePrefix = "swagger";
     });
 }
 
